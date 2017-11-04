1289f805a661929d73619c0d0f8ff58dfa156df1
JonasDch <Jonas.Deichelmann@me.com>
2017-11-03 21:35:15 -0700

    Creating Project

---
+++ FirstResponder/FirstResponder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstResponder
+//
+//  Created by Jonas Deichelmann on 03.11.17.
+//  Copyright © 2017 CSUMB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
