1161aa297a8f48102d8bd59b517bceda54e55a25
JonasDch <Jonas.Deichelmann@me.com>
2017-11-04 09:56:11 -0700

    Compatible to Xcode 8.*

--- FirstResponder/FirstResponder/callViewController.swift
+++ FirstResponder/FirstResponder/callViewController.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class callViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
