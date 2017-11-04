30ecb472af0c158adfc8f08f52aaeb0c91f7b240
Ronny Håland <ronnyhaland@gmail.com>
2017-11-04 10:10:23 -0700

    podfile and an extension

---
+++ FirstResponder/FirstResponder/RegisterUserViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterUserViewController.swift
+//  FirstResponder
+//
+//  Created by Ronny Håland on 11/4/17.
+//  Copyright © 2017 CSUMB. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FirstResponder/Pods/Target Support Files/TB/TB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TB : NSObject
-@end
-@implementation PodsDummy_TB
-@end
