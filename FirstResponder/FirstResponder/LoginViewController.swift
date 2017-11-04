c3814a5142e909a2ad079312e57e4a89799f49bd
JonasDch <Jonas.Deichelmann@me.com>
2017-11-03 22:21:22 -0700

    Added Login Screen in Main Storyboard

---
+++ FirstResponder/FirstResponder/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  FirstResponder
+//
+//  Created by Jonas Deichelmann on 03.11.17.
+//  Copyright © 2017 CSUMB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import TB
+
+class LoginViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
